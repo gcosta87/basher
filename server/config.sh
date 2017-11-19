464f8b3cfff0b081d893c7d05ac63e90643e3a13
Gonzalo G. Costa <gonzalogcosta@yahoo.com.ar>
2017-11-19 02:09:19 -0300

    Ejemplos de configuracion.

--- server/config.sh
+++ server/config.sh
@@ -4,3 +4,20 @@
 # 	Archivo para sobreescribir los valores default y/o
 # 	definir nuevos según sea conveniente
 #
+# raiz del directorio publico: assets, js, public data
+#CONFIG[public_dir_root]='./public'
+
+#raiz del direcotorio recursos  (acceso no publico / no compartidos): hidden files,etc.
+#CONFIG[resources_dir_root]='./resources'
+
+#alias or name of default index file, when user access to a directory
+#CONFIG[default_index_filename]='index.html'
+
+#404 template response
+#CONFIG[template_404]='<!doctype html><html><center>Error 404 :(<br/><small>(Not Found)</small></center></html>'
+#CONFIG[template_fileNotFound]='<!doctype html><html><center>File not found! :(</center></html>'
+
+#CONFIG[template_500]='<!doctype html><html><center>Error 500 :(<br/><small>(Internal Server Error)</small></center></html>'
+
+#Control de la cache en el browser: tiempo (en segundos) par indicar que el contenido deba descartarse. Defualt 15 minutos (54000)
+#CONFIG[cache_refresh_time]=60;
